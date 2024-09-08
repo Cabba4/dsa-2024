d2de6e8fc277e082554c2b1607e7ffee0d39af66
Anmol Arora <anmol.arora@tuni.fi>
2024-09-08 21:33:07 +0300

    recusion

--- wk02_decrease_or_divide/missing/missing2.cc
+++ wk02_decrease_or_divide/missing/missing2.cc
@@ -6,5 +6,14 @@ int searchSmallestMissing(int* A, int left, int right){
     //TODO: your code here
     //return missing value OR
     //return NO_VALUE_MISSING if no value is missing
+    if (left > right) return arr[0] + left;
+
+    int mid = (left + right)/2;
+    if (A[mid] == A[0] + mid){
+        return searchSmallestMissing(A, mid + 1, right);
+    }
+    else {
+        return searchSmallestMissing(A, left, mid - 1);
+    }
     return NO_VALUE_MISSING;
 }
