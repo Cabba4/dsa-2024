984e94b16908276a5f5ba8696691d269c0745d82
Anmol Arora <anmol.arora@tuni.fi>
2024-09-08 20:47:27 +0300

    search smallest

--- wk02_decrease_or_divide/missing/missing.cc
+++ wk02_decrease_or_divide/missing/missing.cc
@@ -6,5 +6,12 @@ int searchSmallestMissingIteration(int* arr, int size){
     //TODO: your code here
     //return missing value OR
     //return NO_VALUE_MISSING if no value is missing
+    if (size == 0) return NO_VALUE_MISSING;
+
+    for(int i=0; i< size; i++) {
+        if( arr[i+1] - arr[i] > 1 ) {
+            return arr[i] + 1;
+        } 
+    }
     return NO_VALUE_MISSING;
 }
