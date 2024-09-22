6d3515b36013a6dbb254032188e00efeb9680108
Anmol Arora <anmol.arora@tuni.fi>
2024-09-22 13:03:02 +0300

    fix invalid 3

--- wk04_trees/iterator_invalidation/invalidation3.cc
+++ wk04_trees/iterator_invalidation/invalidation3.cc
@@ -12,19 +12,13 @@ using namespace std;
  */
 void duplicateEvenRemoveUneven(std::vector<int>& vec) {
 
-    using Iter = std::vector<int>::iterator;
-
-    Iter begin = vec.begin();
-    Iter end = vec.end();
-    for ( Iter it = begin; it < end; it++){
-        if ( *it % 2 == 0 ){
-            vec.insert(it, *it);
-            it++;
-
-        }
-        else {
-            vec.erase(it);
-            it--;
+    auto it = vec.begin();
+    while (it != vec.end()) {
+        if (*it % 2 == 0) {
+            it = vec.insert(it + 1, *it);
+            ++it;
+        } else {
+            it = vec.erase(it);
         }
     }
 }
