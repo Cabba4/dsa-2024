de910d88b6483aa424a925d82c1ac7d356bbd60e
Anmol Arora <anmol.arora@tuni.fi>
2024-09-28 23:12:10 +0300

    reserve memory and push

--- wk05_graphs/improving_functions/improve1.cc
+++ wk05_graphs/improving_functions/improve1.cc
@@ -11,8 +11,9 @@
  */
 std::vector<int> ascendingVector(int n){
     std::vector<int> v;
+    v.reserve(n);
     for ( int i = 0; i < n ; i++){
-        v.insert(v.end(), i);
+        v.push_back(i);
     }
     return v;
 }
